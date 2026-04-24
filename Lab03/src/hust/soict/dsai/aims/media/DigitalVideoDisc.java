74e3186cfb9f66a583aab106a233bdaf05473bac
JingDu06 <Duy.PM2416905@sis.hust.edu.vn>
2026-04-24 15:32:27 +0700

    fix

--- Lab03/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ Lab03/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,47 +1,46 @@
+package hust.soict.dsai.aims.media;
+
 public class DigitalVideoDisc {
+    private static int nbDigitalVideoDiscs = 0;
+
+    private int id;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
 
-    // --- ACCESSORS (Getters) ---
+    public int getId() { return id; }
     public String getTitle() { return title; }
-    public String getCategory() { return category; }
-    public String getDirector() { return director; }
-    public int getLength() { return length; }
     public float getCost() { return cost; }
 
-    // --- CONSTRUCTORS ---
-
-    // 1. Create a DVD object by title
     public DigitalVideoDisc(String title) {
         this.title = title;
+        this.id = ++nbDigitalVideoDiscs;
     }
 
-    // 2. Create a DVD object by category, title and cost
     public DigitalVideoDisc(String category, String title, float cost) {
         this(title);
         this.category = category;
         this.cost = cost;
     }
 
-    // 3. Create a DVD object by director, category, title and cost
     public DigitalVideoDisc(String director, String category, String title, float cost) {
         this(category, title, cost);
         this.director = director;
     }
 
-    // 4. Create a DVD object by all attributes: title, category, director, length and cost
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        
-        this(director, category, title, cost); 
+        this(director, category, title, cost);
         this.length = length;
     }
-<<<<<<< HEAD
-=======
-    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList) {
-    	//Over-loading
+
+    public boolean isMatch(String title) {
+        return this.title.toLowerCase().contains(title.toLowerCase());
     }
->>>>>>> 920816d (add overloading)
+
+    @Override
+    public String toString() {
+        return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+    }
 }
